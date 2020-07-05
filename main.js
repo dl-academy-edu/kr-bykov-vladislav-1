5e5b4f2c2fac570fbfdb611314a4027b231f7d21
Vladislav Bykov <therockshade@gmail.com>
2020-07-06 02:46:15 +0400

    feat: add open and close functions for popup

---
+++ main.js
@@ -0,0 +1,20 @@
+let modalOpen = document.querySelector('.button--slider'),
+		modalClose = document.querySelector('.close'),
+		popup = document.querySelector('.popup-bg'),
+		input = document.querySelector('.input_focus');
+
+modalOpen.addEventListener('click', function() {
+	popup.classList.add('popup_open');
+	input.focus();
+});
+
+modalClose.addEventListener('click', function() {
+	popup.classList.remove('popup_open');
+	modalOpen.focus();
+});
+
+window.addEventListener('keydown', function(e) {
+	if (e.code === 'Escape' && popup.classList.contains('popup_open')) {
+		popup.classList.remove('popup_open');
+	}
+});
